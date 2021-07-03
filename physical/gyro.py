0ce1b213fbe9da69639f3f2f90787a135e623d59
Ian Webster <IanWebster2828@gmail.com>
2021-07-03 15:42:34 -0400

    rename legacy tracker, add gyro functions from MinIMU library.

--- physical/gyro.py
+++ physical/gyro.py
@@ -1,2 +1,17 @@
-def get_tracker_gyro():
-    print(f"Gyro reading: x: {x} y: {y} z: {z}")
+from MinIMU_v5_pi import MinIMU_v5_pi
+
+class TrackerGyro():
+    def __init__(self):
+        """
+        Initialize IMU for gyro functionality
+        """
+        self.IMU = MinIMU_v5_pi()
+        self.IMU.trackAngle()
+
+    def get_tracker_gyro(self):
+        """
+        Return poll result for ENU_spherical angles from Antenna Tracker gyro
+        in format [x_axis, y_axis, z_axis]
+        """
+        # Todo: higher poll rate, and read average of prev. queue of data points.
+        return self.IMU.prevAngle[0]
