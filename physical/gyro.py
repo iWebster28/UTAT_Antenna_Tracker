5507fbfef875c810fef4a17048c6adb7cff30fe7
Ian <IanWebster2828@gmail.com>
2021-07-03 15:47:54 -0400

    merge local

--- physical/gyro.py
+++ physical/gyro.py
@@ -1,2 +1,18 @@
-def get_tracker_gyro():
-    print(f"Gyro reading: x: {x} y: {y} z: {z}")
+from MinIMU_v5_pi import MinIMU_v5_pi
+
+class gryo():
+    def __init__(self):
+        self.IMU = MinIMU_v5_pi()
+        self.IMU.trackAngle()
+
+    def get_tracker_gyro(self):
+        """
+        Poll ENU_spherical angles from Antenna Tracker gyro
+        """
+        return self.IMU.prevAngle[0]
+
+def get_tracker_drone_delta(delta_t2d_ENU_spherical, tracker_gyro_ENU_spherical):
+    """
+    Determine the delta between tracker and drone positions in ENU_spherical
+    """
+    return (delta_t2d_ENU_spherical - tracker_gyro_ENU_spherical)
